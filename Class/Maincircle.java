9fa59e574791d53b7b7698c1cdb48c364fff8731
Dinda Ayu Puspaningrum Marhono <143941161+Dmarhono@users.noreply.github.com>
2025-03-21 13:37:15 +0700

    Update Maincircle.java

--- Class/Maincircle.java
+++ Class/Maincircle.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package TClass;
 
-/**
- *
- * @author LENOVO
- */
 public class Maincircle {
      public static void main(String[] args){
      Circle bulat1 = new Circle();   
