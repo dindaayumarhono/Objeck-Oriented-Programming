8e21e6e4a60e2b7e7547de8f231e1e67d1bad9da
Dinda Ayu Puspaningrum Marhono <143941161+Dmarhono@users.noreply.github.com>
2025-03-21 13:38:50 +0700

    Update Square.java

--- Class/Square.java
+++ Class/Square.java
@@ -1,14 +1,5 @@
 package TClass;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author LENOVO
- */
 public class Square {
     public int sisi;
     
