e2d9ff42da5e861ac047b25b20095932cf15396e
Dinda Ayu Puspaningrum Marhono <143941161+Dmarhono@users.noreply.github.com>
2025-03-21 13:38:15 +0700

    Update Mainsquare.java

--- Class/Mainsquare.java
+++ Class/Mainsquare.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package TClass;
 
-/**
- *
- * @author LENOVO
- */
 public class Mainsquare {
     public static void main(String[] args){
      Square kotak1 = new Square();   
