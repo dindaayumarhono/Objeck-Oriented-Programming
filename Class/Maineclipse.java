203686c57a5827aa37b10418dc4cafc0467a15f9
Dinda Ayu Puspaningrum Marhono <143941161+Dmarhono@users.noreply.github.com>
2025-03-21 13:37:34 +0700

    Update Maineclipse.java

--- Class/Maineclipse.java
+++ Class/Maineclipse.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package TClass;
 
-/**
- *
- * @author LENOVO
- */
 public class Maineclipse {
     public static void main(String[] args){
     Eclipse oval1 = new Eclipse();
