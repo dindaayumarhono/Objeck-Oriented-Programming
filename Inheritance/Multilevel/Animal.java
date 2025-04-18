15c59f667f351c79257e12de15d7eb80e889c1a6
Dinda Ayu Puspaningrum Marhono <143941161+Dmarhono@users.noreply.github.com>
2025-04-18 21:10:05 +0700

    Update Animal.java

--- Inheritance/Multilevel/Animal.java
+++ Inheritance/Multilevel/Animal.java
@@ -1,1 +1,18 @@
+package inheritance.multilevel;
 
+public class Animal {
+    public Animal(){
+        System.out.println("i am animal");
+    }
+    public void speak(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args){
+        Bird b = new Bird();
+        b.speak();
+        b.fly();
+        Penguin p = new Penguin();
+        p.speak();
+        p.fly();
+    }
+}
